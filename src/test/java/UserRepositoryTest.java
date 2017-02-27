d494bfd62abb3fc4cbae696ce1bef051e96d65d2
Max Matyushin <Fliplaudex@mail.ru>
2017-02-27 20:34:28 +0400

    UserRepository

--- src/test/java/UserRepositoryTest.java
+++ src/test/java/UserRepositoryTest.java
@@ -4,12 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import ru.entity.*;
 import org.junit.Test;
 import ru.repository.UserRepository;
+import ru.specifications.EmptySpecification;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,19 +24,19 @@ import static org.junit.Assert.*;
 @ContextConfiguration(locations = {"/beans.xml"})
 @Transactional
 public class UserRepositoryTest {
-
-    @Autowired
     JdbcTemplate jdbcTemplate;
-    @Autowired
-    UserRepository rep;
-
     ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Test
     public void addUser(){
         User user = new User(200,"Test name",false);
-        rep.addUser(user);
+        String sqlQuery = String.format("INSERT INTO users (user_id, name,is_admin) VALUES (%s,\'%s\',%b);",user.getUserId(),user.getName(),user.getIsAdmin());
+        jdbcTemplate.update(sqlQuery);
         String sqlQuery2 = String.format("SELECT * FROM users WHERE user_id = %s",user.getUserId());
         List<User> testUser = this.jdbcTemplate.query(sqlQuery2, new RowMapper<User>(){
             public User mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -52,7 +55,8 @@ public class UserRepositoryTest {
     @Test
     public void removeUser() throws Exception {
         User user = new User(11,"Test name",false);
-        rep.removeUser(user);
+        String sqlQuery = String.format("DELETE FROM users WHERE user_id = %s;",user.getUserId());
+        jdbcTemplate.update(sqlQuery);
         String sqlQuery2 = String.format("SELECT * FROM users WHERE user_id = %s",user.getUserId());
         List<User> testUser = this.jdbcTemplate.query(sqlQuery2, new RowMapper<User>(){
             public User mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -65,11 +69,12 @@ public class UserRepositoryTest {
         });
         assertEquals(testUser.size(),0);
     }
-
+    
     @Test
     public void updateUser() throws Exception {
         User user = new User(11,"Test name1",true);
-        rep.updateUser(user);
+        String sqlQuery = String.format("UPDATE users SET is_admin = %b, name = \'%s\' WHERE user_id = %s;",user.getIsAdmin(),user.getName(),user.getUserId());
+        jdbcTemplate.update(sqlQuery);
         String sqlQuery2 = String.format("SELECT * FROM users WHERE user_id = %s",user.getUserId());
         List<User> testUser = this.jdbcTemplate.query(sqlQuery2, new RowMapper<User>(){
             public User mapRow(ResultSet rs, int rowNum) throws SQLException {
