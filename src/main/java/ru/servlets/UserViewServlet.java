10f2b7975c69f537a3ca5afc5b9329e11d71e4c5
Max Matyushin <Fliplaudex@mail.ru>
2017-04-02 19:59:33 +0400

    userViewServlet commit

--- src/main/java/ru/servlets/UserViewServlet.java
+++ src/main/java/ru/servlets/UserViewServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "UserViewServlet")
@@ -15,6 +16,16 @@ public class UserViewServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        request.getRequestDispatcher("user/UserView.jsp").include(request,response);
+        HttpSession session = request.getSession();
+        if (session.getAttribute("admin") != null) {
+            boolean isAdmin = (Boolean) session.getAttribute("admin");
+            if (!isAdmin) {
+                response.sendRedirect("/offerView");
+            } else {
+                response.sendRedirect("/adminPanel");
+            }
+        } else {
+            request.getRequestDispatcher("user/UserView.jsp").include(request,response);
+        }
     }
 }
